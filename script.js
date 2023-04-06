ecb9fc3215ced01a9c27ad24b10a0eeb22994bf6
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-04-06 15:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function abc(){
+let promise = new Promise(function(resolve,reject){
+
+	setTimeout(()=>{
+		resolve("Hello, world!");
+	},1000))
+
+	return promise;
+	
+}
+	
+	
+}
+
+abc().then(function(msg){
+	document.getElementById("output").innerText=msg;
+})
